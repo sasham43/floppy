b55a42e3e53008553277f673ff557323877abdb9
sasham43 <sashasemail@gmail.com>
2018-04-09 19:39:08 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+var Cvlc   = require('cvlc'),
+    player = new Cvlc(),
+    fs     = require('fs');
+
+
+
+player.cmd('longhelp', function gotCommands(err, response) {
+    console.log('Available commands: ' + response);
+});
+
+var tracks = fs.readdirSync('/media/pi/0');
+
+var track = '/media/pi/0/' + tracks[0];
+
+player.play(track, function startedLocalFile() {
+    // The file has started playing
+    console.log('playing');
+});
