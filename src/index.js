cc1a54bc66c842f88c723fc44390cd286b67bc21
sasham43 <sashasemail@gmail.com>
2018-04-09 20:04:47 -0500

    make express server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ var Cvlc   = require('cvlc'),
     player = new Cvlc(),
     fs     = require('fs');
 
+var express = require('express');
 
+var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+  limit: '50mb'
+}));
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+
+var listenPort = process.env.PORT || 3000;
 
 player.cmd('longhelp', function gotCommands(err, response) {
     console.log('Available commands: ' + response);
@@ -21,4 +33,41 @@ tracks.forEach(function(t){
 player.play(track, function startedLocalFile() {
     // The file has started playing
     console.log('playing', track);
+});
+
+app.use(function(err, req, res, next){
+  console.log('error:', err);
+  res.status(err.statusCode || 500).json(err);
+});
+
+app.get('/pause', function(req, res, next){
+    player.cmd('pause', function(paused){
+        console.log('paused', paused);
+        res.send('paused')
+    });
+});
+
+app.get('/play', function(){
+    player.cmd('play', function(play){
+        console.log('playing', play);
+        res.send('playing')
+    });
+});
+
+app.get('/fwd', function(){
+    player.cmd('seek +15s', function(fwd){
+        console.log('fwd', fwd);
+        res.send('fwd')
+    });
+});
+
+app.get('/rew', function(){
+    player.cmd('seek -15s', function(rew){
+        console.log('rew', rew);
+        res.send('rew')
+    })
+})
+
+app.listen(listenPort, function(){
+  console.log('server listening on port', listenPort + '...');
 });
