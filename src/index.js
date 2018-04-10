39da5fe7e79f7210ec12b14da3bd82650b8926d7
sasham43 <sashasemail@gmail.com>
2018-04-09 19:57:27 -0500

    find

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ player.cmd('longhelp', function gotCommands(err, response) {
 
 var tracks = fs.readdirSync('/media/pi/0');
 
-var track = '/media/pi/0/' + tracks[0];
+// var track = '/media/pi/0/' + tracks[0];
+var track;
+tracks.forEach(function(t){
+    if(t.includes('.opus')){
+        track = t;
+    }
+});
 
 player.play(track, function startedLocalFile() {
     // The file has started playing
