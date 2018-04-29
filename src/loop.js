1dcd44a27a3beaa9b5c166861af97851d5fb57a5
sasham43 <sashasemail@gmail.com>
2018-04-29 13:35:18 -0500

    both

--- src/loop.js
+++ src/loop.js
@@ -18,9 +18,12 @@ const cp = require('child_process');
 
 // remember to wipe the log file when this starts up
 
+// var mode = 'add/remove';
+var mode = 'change';
+
 var last_cmd = '';
-// var prev_evts = [];
-// var status = '';
+var prev_evts = [];
+var status = '';
 
 setInterval(checkFile, 5000);
 
@@ -30,100 +33,104 @@ function checkFile(){
             console.log('err', err);
 
         var lines = resp.split('\n');
-        // var new_evts = [];
+        var new_evts = [];
         var evts = [];
         lines.forEach(function(line){
             var split = line.split(' ');
             var type = split[1];
             var syspath = split[split.length - 2];
-            if ((type == 'add' || type == 'remove') && syspath.includes('sda1')) {
-                evts.push(type);
+            if(mode == 'add/remove'){
+                if ((type == 'add' || type == 'remove') && syspath.includes('sda1')) {
+                    evts.push(type);
+                }
+            } else if (mode == 'change'){
+                new_evts.push(type);
             }
-            // evts.push(type);
-            // new_evts.push(type);
         });
 
-        // if(new_evts.length != prev_evts.length){
-        //     if(status == 'mounted'){
-        //         // unmount that shizz
-        //         let umount = cp.spawn('pumount', ['/dev/sda1']);
-        //
-        //         umount.stdout.on('data', (data) => {
-        //           console.log(`umount stdout: ${data}`);
-        //
-        //           // switch
-        //           status = 'unmounted';
-        //           prev_evts = new_evts;
-        //         });
-        //
-        //         umount.stderr.on('data', (data) => {
-        //           console.log(`umount stderr: ${data}`);
-        //         });
-        //
-        //         umount.on('close', (code) => {
-        //           console.log(`umount child process exited with code ${code}`);
-        //         });
-        //
-        //
-        //     } else if (status == 'unmounted'){
-        //         // go mount it
-        //         let mount = cp.spawn('pmount', ['/dev/sda1']);
-        //
-        //         mount.stdout.on('data', (data) => {
-        //           console.log(`mount stdout: ${data}`);
-        //
-        //           // switch
-        //           status = 'mounted';
-        //           prev_evts = new_evts;
-        //         });
-        //
-        //         mount.stderr.on('data', (data) => {
-        //           console.log(`mount stderr: ${data}`);
-        //         });
-        //
-        //         mount.on('close', (code) => {
-        //           console.log(`mount child process exited with code ${code}`);
-        //         });
-        //     }
-        // }
+        if(mode == 'change'){
+            if(new_evts.length != prev_evts.length){
+                if(status == 'mounted'){
+                    // unmount that shizz
+                    let umount = cp.spawn('pumount', ['/dev/sda1']);
 
-        // last_cmd = evts[evts.length - 1];
-        console.log('running', evts[evts.length - 1], last_cmd);
-        if (last_cmd != evts[evts.length - 1]) {
-            console.log('last cmd', last_cmd);
-            // new cmd
-            if (evts[evts.length - 1] == 'add') {
-                console.log('mount');
-                let mount = cp.spawn('pmount', ['/dev/sda1']);
+                    umount.stdout.on('data', (data) => {
+                      console.log(`umount stdout: ${data}`);
 
-                mount.stdout.on('data', (data) => {
-                  console.log(`mount stdout: ${data}`);
-                });
+                      // switch
+                      status = 'unmounted';
+                      prev_evts = new_evts;
+                    });
 
-                mount.stderr.on('data', (data) => {
-                  console.log(`mount stderr: ${data}`);
-                });
+                    umount.stderr.on('data', (data) => {
+                      console.log(`umount stderr: ${data}`);
+                    });
 
-                mount.on('close', (code) => {
-                  console.log(`mount child process exited with code ${code}`);
-                });
-            } else if (evts[evts.length - 1] == 'remove') {
-                console.log('umount');
-                let umount = cp.spawn('pumount', ['/dev/sda1']);
+                    umount.on('close', (code) => {
+                      console.log(`umount child process exited with code ${code}`);
+                    });
 
-                umount.stdout.on('data', (data) => {
-                  console.log(`umount stdout: ${data}`);
-                });
 
-                umount.stderr.on('data', (data) => {
-                  console.log(`umount stderr: ${data}`);
-                });
+                } else if (status == 'unmounted'){
+                    // go mount it
+                    let mount = cp.spawn('pmount', ['/dev/sda1']);
 
-                umount.on('close', (code) => {
-                  console.log(`umount child process exited with code ${code}`);
-                });
+                    mount.stdout.on('data', (data) => {
+                      console.log(`mount stdout: ${data}`);
+
+                      // switch
+                      status = 'mounted';
+                      prev_evts = new_evts;
+                    });
+
+                    mount.stderr.on('data', (data) => {
+                      console.log(`mount stderr: ${data}`);
+                    });
+
+                    mount.on('close', (code) => {
+                      console.log(`mount child process exited with code ${code}`);
+                    });
+                }
             }
-            last_cmd = evts[evts.length - 1];
+        } else if (mode == 'add/remove'){
+            console.log('running', evts[evts.length - 1], last_cmd);
+            
+            if (last_cmd != evts[evts.length - 1]) {
+                console.log('last cmd', last_cmd);
+                // new cmd
+                if (evts[evts.length - 1] == 'add') {
+                    console.log('mount');
+                    let mount = cp.spawn('pmount', ['/dev/sda1']);
+
+                    mount.stdout.on('data', (data) => {
+                      console.log(`mount stdout: ${data}`);
+                    });
+
+                    mount.stderr.on('data', (data) => {
+                      console.log(`mount stderr: ${data}`);
+                    });
+
+                    mount.on('close', (code) => {
+                      console.log(`mount child process exited with code ${code}`);
+                    });
+                } else if (evts[evts.length - 1] == 'remove') {
+                    console.log('umount');
+                    let umount = cp.spawn('pumount', ['/dev/sda1']);
+
+                    umount.stdout.on('data', (data) => {
+                      console.log(`umount stdout: ${data}`);
+                    });
+
+                    umount.stderr.on('data', (data) => {
+                      console.log(`umount stderr: ${data}`);
+                    });
+
+                    umount.on('close', (code) => {
+                      console.log(`umount child process exited with code ${code}`);
+                    });
+                }
+                last_cmd = evts[evts.length - 1];
+            }
         }
     });
 }
