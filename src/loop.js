f8b39e14e774ca771ae4ef917385f7759410e265
sasham43 <sashasemail@gmail.com>
2018-04-29 14:12:50 -0500

    t

--- src/loop.js
+++ src/loop.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
-const filesysem = require('fs-filesystem');
+const filesystem = require('fs-filesystem');
 
 // var devices = filesystem(null, (err, data)=>{
 //     if(err)
