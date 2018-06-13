f18d9aecb89be5316aea984cbf0e7cdd2339e361
sasham43 <sashasemail@gmail.com>
2018-06-12 20:29:36 -0500

    quote

--- src/loop.js
+++ src/loop.js
@@ -124,7 +124,7 @@ function playSongs(songs, index) {
     //         checkPlayerStatus(player_status_promise, songs, new_index)
     //     }, 1000);
     // })
-    player.cmd(`play ${songs[index]} --file-caching 10000`, function(){
+    player.cmd(`play '${songs[index]}' --file-caching 10000`, function(){
         console.log(`playing track ${songs[index]} ${index}`);
         player_status_promise = setInterval(function(){
             var new_index = (index >= songs.length - 1) ? index + 1 : -1; // send a -1 if this is the last track, otherwise increment track
